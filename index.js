f6a07e1929883c3811b2fd72952eca26769eaa62
Rivadeneiea <joserivadeneira538@gmail.com>
2023-08-26 21:23:51 -0300

    agregamos un nuevo puerto

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
-console.log("esto es una prueba");
-// 1- configuraciones iniciales
+console.log("esto es una prueba kakak");
 
+// 1- configuraciones iniciales
+const app = express();
+// crear una variable
+app.set("port", process.env.PORT || 4000);
 // middlewares
 
 // crear rutas
