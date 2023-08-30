2476d397ac6b5f12bcb4facbc1b949969e46291c
Rivadeneiea <joserivadeneira538@gmail.com>
2023-08-30 20:11:31 -0300

    agregamos un puerto al backend

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ console.log("esto es una prueba kakak");
 const app = express();
 // crear una variable
 app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+  console.log("estoy en un puerto" + app.get("port"));
+});
 // middlewares
 
 // crear rutas
