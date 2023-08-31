3a72d1e8c7aba623dfe56637f3d7e8aca656e4ce
Rivadeneiea <joserivadeneira538@gmail.com>
2023-08-30 21:32:09 -0300

    agregamos rutas

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 console.log("esto es una prueba kakak");
-
+import cors from "cors";
+import morgan from "morgan";
+import "dotenv/config"; //permite procesar variables de entorno
+import productoRouter from "./src/routes/productos.routes.js";
 // 1- configuraciones iniciales
 const app = express();
 // crear una variable
@@ -8,6 +11,13 @@ app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
   console.log("estoy en un puerto" + app.get("port"));
 });
-// middlewares
+// middlewares : funciones se agregan antes de las rutas
 
+app.use(cors()); //permite conexiones remotas
+app.use(express.json()); //permite interpretar datos en formato json
+app.use(express.urlencoded({ extended: true })); //ayuda a interpretar datos
+//del body del request
+app.use(morgan("dev")); //nos da mas informacion en la terminal
 // crear rutas
+//http://localjost:4000/api/producto
+app.use("/api", productoRouter);
---
+++ src/database/database.js
@@ -0,0 +1,3 @@
+//mongodb+srv://joserivadeneira538:<password>@cluster0.eaxssl8.mongodb.net/
+//joserivadeneira538
+//k5XMqihCKy3EhRsq
