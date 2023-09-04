91cd82ea452c90ce3ecb1018c07ae582e05bb69d
Rivadeneiea <joserivadeneira538@gmail.com>
2023-09-04 20:31:36 -0300

    agregamos el modelo de producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,3 +1,8 @@
+import Producto from "../models/producto.js";
+
 export const listarProductos = (req, res) => {
-  res.send("esto es una prueba");
+  try {
+    // ir a pedir a la BD
+    res.send("esto es una prueba");
+  } catch (error) {}
 };
