dd94ce1f9d5bb06e2af3e63cfd9abc68e29bb3d5
Rivadeneiea <joserivadeneira538@gmail.com>
2023-09-04 20:05:37 -0300

    agregamos conexion a BD

--- src/database/database.js
+++ src/database/database.js
@@ -1,3 +1,10 @@
-//mongodb+srv://joserivadeneira538:<password>@cluster0.eaxssl8.mongodb.net/
-//joserivadeneira538
-//k5XMqihCKy3EhRsq
+import mongoose, { connect } from "mongoose";
+import "dotenv/config";
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI);
+
+const datosConexion = mongoose.connection;
+datosConexion.once("open", () => {
+  console.log("BD conectada");
+});
