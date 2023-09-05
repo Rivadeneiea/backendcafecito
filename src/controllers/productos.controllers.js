0ee203908cfd2ec14b57e04e6151d0f79b42b71a
Rivadeneiea <joserivadeneira538@gmail.com>
2023-09-04 21:21:37 -0300

    agregamos un producto en la BD

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -6,3 +6,22 @@ export const listarProductos = (req, res) => {
     res.send("esto es una prueba");
   } catch (error) {}
 };
+
+export const crearProducto = async (req, res) => {
+  console.log(req.body);
+  try {
+    // ir a pedir a la BD
+
+    const productoNuevo = new Producto(req.body);
+    // guardar el producto nuevo en la BD
+    await productoNuevo.save();
+    res.status(201).json({
+      mensaje: "elprocto fue creado correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "el procto no fue creado correctamente",
+    });
+  }
+};
