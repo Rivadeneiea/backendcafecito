f382feb5da8ac16356d69bfffa985897d5132495
Rivadeneiea <joserivadeneira538@gmail.com>
2023-09-11 21:27:15 -0300

    agregamos un archivo estatico

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ import "dotenv/config"; //permite procesar variables de entorno
 import productoRouter from "./src/routes/productos.routes.js";
 import usuarioRouter from "./src/routes/usuario.routes.js";
 import "./src/database/database.js";
+import path, { join } from "path";
+import { fileURLToPath } from "url";
 // 1- configuraciones iniciales
 const app = express();
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
 // crear una variable
 app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
@@ -20,7 +24,22 @@ app.use(express.json()); //permite interpretar datos en formato json
 app.use(express.urlencoded({ extended: true })); //ayuda a interpretar datos
 //del body del request
 app.use(morgan("dev")); //nos da mas informacion en la terminal
+app.use(express.static(path.join(_dirname, "/public")));
+// console.log(
+//   path.join(
+//     __dirname,
+//     "C:UsersDellOneDriveEscritorio\backendcafecito",
+//     "/public"
+//   )
+// );
+// app.use(
+//   express.static(
+//     path.join("C:UsersDellOneDriveEscritorio\backendcafecito", "/public")
+//   )
+// );
 // crear rutas
+// agregar un archivo estatico
+
 //http://localjost:4000/api/producto
 app.use("/api", productoRouter);
 app.use("/api/usuario", usuarioRouter);
